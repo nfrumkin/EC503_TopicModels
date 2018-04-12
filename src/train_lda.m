d6caf7be2e68b520dea93836050f0232a3d80f30
nfrumkin <natasha.frumkin@gmail.com>
2018-04-11 20:38:51 -0400

    update LDA training

--- src/train_lda.m
+++ src/train_lda.m
@@ -2,8 +2,10 @@ clear
 clc all;
 
 % load data
-[vocab] = textread('../data/vocab.txt','%s');
-[doc_id_train, word_id_train, wc_train] = textread('../data/docword.nytimes.txt','%d %d %d');
+fprintf("======= load data\n")
+[vocab] = textread('../../data/vocab.txt','%s');
+[doc_id_train, word_id_train, wc_train] = textread('../../data/docword.nytimes.txt','%d %d %d');
+
 
 % trim away top lines
 doc_id_train = doc_id_train(4:end, :);
@@ -13,30 +15,46 @@ wc_train = wc_train(4:end, :);
 num_vocab = length(vocab);
 num_docs = length(unique(doc_id_train));
 
-% allocate sparse matrix for counts
-counts_matrix = spalloc(num_docs, num_vocab,num_docs*1000);
-
 % convert data to count matrix, need to use batches so matlab can handle
 % data
+fprintf("======= create counts matrix")
 batch_size =  int32(num_docs/100);
-for i = 1:100
-   i
-   
-   % convert doc id and word id to row and col vals
-   ind = sub2ind(size(counts_matrix), doc_id_train((i-1)*batch_size+1:i*batch_size), word_id_train((i-1)*batch_size+1:i*batch_size));
-   
-   % index into counts matrix and place appropriate word count
-   counts_matrix(ind) = wc_train((i-1)*batch_size+1:i*batch_size);
+counts_matrix = sparse(doc_id_train, word_id_train, wc_train);
+
+
+num_topics = [10;20;50;100;200];
+for i = 1:4
+    i
+    fprintf("======= generate LDA model")
+    model_cgs(i) = fitlda(counts_matrix(1:50000,:), num_topics(i,1), "Solver","cgs");
 end
 
-num_topics = 10;
-mdl = fitlda(counts_matrix(1:15000,:), num_topics);
+num_topics = [10;20;50;100;200];
+for i = 1:4
+    i
+    fprintf("======= generate LDA model")
+    model_cgs(i) = fitlda(counts_matrix(1:50000,:), num_topics(i,1), "Solver","avb");
+end
 
-% generate word cloud for visualization
+generate word cloud for visualization
 figure
-topic_words = vocab(str2double(mdl.Vocabulary));
-for topicIdx = 1:num_topics
-    subplot(2,2,topicIdx)
-    wordcloud(topic_words,mdl.TopicWordProbabilities(:,topicIdx));
-    title("Topic: " + topicIdx)
+test_doc = 3957;
+
+for i = 1:5
+    [topic_pred, num] = predict(model(1),counts_matrix(test_doc,:))
+    topic_words = vocab(str2double(model_avb.Vocabulary));
+    for topicIdx = topic_pred(1)%1:num_topics
+        subplot(3,2,i)
+        wordcloud(topic_words,model_avb(i).TopicWordProbabilities(:,topicIdx));
+        title("Number of Topics: " + num_topics(i))
+    end
 end
+
+subplot(3,2,6)
+[B, I] = maxk(counts_matrix(test_doc,:),10);
+bar(B)
+title("Most Frequent Words in Document")
+ylabel("Frequency")
+set(gca,'xticklabel', vocab(I))
+xtickangle(45);
+
