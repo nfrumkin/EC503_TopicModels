4b5b7ad34dba7581030b0c7d44ac62f1af48f52e
Richard Yu <37787489+f22604026@users.noreply.github.com>
2018-04-22 12:42:44 -0400

    Add files via upload

--- src/shuffling_by_hungairan.m
+++ src/shuffling_by_hungairan.m
@@ -1,15 +1,15 @@
 function [beta_syn_reordered,cost]=shuffling_by_hungairan(beta,beta_hat)
 
 Ntopic=size(beta,2);
+
 for iTopic=1:Ntopic
-    cost_M(:,iTopic)=sum(abs(beta_hat-beta(:,iTopic)))';  %l1 dist
+    cost_M(iTopic,:)=sum(abs(beta_hat-beta(:,iTopic)));  %l1 dist
 end
 
 [assignment,cost] = munkres(cost_M);
 
 for iTopic=1:Ntopic
     beta_syn_reordered(:,iTopic)=beta_hat(:,assignment(iTopic));
-    
 end
 
 end
